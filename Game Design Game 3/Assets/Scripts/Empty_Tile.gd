f9a9c8e25512010ef181fa07e71b123bab35d815
Robert W <robertwesleyward2019@gmail.com>
2022-04-19 15:21:28 -0500

    Rudimentary Placement
    +added ability to place straight cables
    +created buttons to select cables
    +Added some art for cables and client

--- Game Design Game 3/Assets/Scripts/Empty_Tile.gd
+++ Game Design Game 3/Assets/Scripts/Empty_Tile.gd
@@ -1,16 +1,19 @@
 extends Node2D
 export var destroyable = true
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+export var baseTile = true
+export var tile_cost = 0
+export var adjacentTiles = []
+var Board 
+var Inventory 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	Board = get_parent()
+	Inventory= get_parent().get_parent().get_node("Inventory")
 
-
+func getAdjacentTiles():
+	
+	pass
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
@@ -19,8 +22,10 @@ func _ready():
 func _on_Area2D_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton:
 		if event.is_pressed():
-			if destroyable:
-				queue_free()
+			if !baseTile:
+				if (Inventory.selectedTile != "NULL"):					
+					Board.placeTile(self.position)
+					queue_free()
 			else:
 				return
 			print(shape_idx)
---
+++ Game Design Game 3/Assets/Scripts/Item_button.gd
@@ -0,0 +1,9 @@
+extends Node
+var InventoryScript
+func _ready():
+	InventoryScript = get_parent()
+func _on_Button_pressed(extra_arg_0):
+	InventoryScript.selectedTile =str(extra_arg_0)
+	print(InventoryScript.selectedTile)
+
+
